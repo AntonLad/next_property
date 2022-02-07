66e4bb93931e45ccaa58ac8bba722955d3287580
dadhy <evgenzolotoff@gmail.com>
2022-02-08 01:12:25 +0400

    pagination2

--- pages/object/[cadnumber].js
+++ pages/object/[cadnumber].js
@@ -1,7 +1,16 @@
+import { Divider } from "@material-ui/core"
 import { useRouter } from "next/router"
 
 export default function Object() {
   const router = useRouter()
+  const info = router.query.cadnumber
   console.log('ROUTER', router.query.cadnumber)
-  return <h1>{router.query.cadnumber}</h1>
+  const LocalFlatData = JSON.parse(localStorage.getItem(`${info}`))
+  return (
+    <div>
+      {JSON.stringify(LocalFlatData)}
+    </div>
+  )
+
+
 }
