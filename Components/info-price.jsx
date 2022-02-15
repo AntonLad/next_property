e9b3b54d9e719549b2975d10e79b0bf1f9349b1a
dadhy <evgenzolotoff@gmail.com>
2022-02-15 21:29:59 +0400

    some fix in css

--- Components/info-price.jsx
+++ Components/info-price.jsx
@@ -6,8 +6,8 @@ const Price = ({ cadastrObj }) => {
   const meterPrice = cadastrObj?.price?.stats
 
   return (
-    <div>
-      <div data-content="main" className="object__block" id="price-info">
+    <div data-content="main" className="object__block" id="price-info">
+      <div className="object__block-wrap">
         <div className="object__block-title _price">
           Рыночная стоимость обекта недвижимости
         </div>
@@ -49,70 +49,11 @@ const Price = ({ cadastrObj }) => {
                 <div className="object__blockTableTd">Максимальная цена:</div>
                 <div className="object__blockTableTd">{`${priceRange[1]} руб.`}</div>
               </div>
-            </div> 
+            </div>
           </>
         )}
-          {/* {objectId && (
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Кадастровый номер:</div>
-            <div className="object__blockTableTd">{objectId}</div>
-          </div>
-          )}
-          {(objectName || name) && (
-            <div className="object__blockTableTr">
-              <div className="object__blockTableTd">Тип объекта:</div>
-              <div className="object__blockTableTd">{objectName || name}</div>
-            </div>)}
-          {areaValue && (
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Площадь:</div>
-            <div className="object__blockTableTd">{areaValue}</div>
-          </div>
-          )} */}
-          {/* <div data-content="owners" id="price-info" className="object__block">
-    <div className="object__block-load">
-      <div className="loader__table">
-        <div className="loader__td">
-          <div className="loader__img"> </div>
-        </div>
       </div>
     </div>
-    <div className="object__block-wrap">
-      <div className="object__block-title _price">Рыночная стоимость объекта недвижимости</div>
-      <div className="object__block-date">от (дата)</div>
-      <div className="clearfix"> </div>
-      <div className="object__block-data">
-        <div className="object__block-data-count">Стоимость объекта</div>
-        <div className="object__block-data-wrap">
-          <div className="object__block-data-item">
-            <div className="object__block-data-item-title">
-              стоимость кв метра
-            </div>
-            <div className="object__block-data-item-lock">
-              <div className="object__block-data-item-lock-block">Данные в отчёте</div>
-            </div>
-            <div className="object__block-data-item-text">
-              {' '}
-              Дата регистрации:
-              <b>25.07.2007</b>Свидетельство о регистрации:
-              <b>№**-**-30/052/*00*-359</b>
-              Форма владения:
-              <b>Собственность</b>
-              Во владении:
-              <b className="_success"> 14 лет 5 месяцев 29 дней </b>
-              Основание:
-              <b>************* ******* *************</b>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> */}
-      </div>
-    </div>
-
-    
-
   )
 }
 
