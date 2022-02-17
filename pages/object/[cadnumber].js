576253b681ce34eec68b1138079611780bcc47bc
dadhy <evgenzolotoff@gmail.com>
2022-02-17 23:11:24 +0400

    some fix

--- pages/object/[cadnumber].js
+++ pages/object/[cadnumber].js
@@ -20,82 +20,18 @@ export default function Object({ cadastralObject }) {
   const props = JSON.parse(cadastralObject)
   const addressNotes = props?.objectData?.objectAddress?.addressNotes || props?.objectData?.objectAddress?.mergedAddress
   console.log('addressNotes', addressNotes)
+
   return (
-      <div>
-        <Meta
-          title={`Проверка объекта недвижимости онлайн. Кадастровый номер: ${cadNumber}, адрес: ${addressNotes}`}
-          descritoin={`Проверка объекта недвижимости онлайн по кадастровому номеру ${cadNumber} | Проверка объекта недвижимости онлайн по адресу: ${addressNotes}`}
-          keywords={`${cadNumber}, проверка недвижимости, ЕГРН, Росреестр`}
-        />
-        <InfoAppart cadastrObj={JSON.parse(cadastralObject)}/>
-      </div>
+    <div>
+      <Meta
+        title={`Проверка объекта недвижимости онлайн. Кадастровый номер: ${cadNumber}, адрес: ${addressNotes}`}
+        descritoin={`Проверка объекта недвижимости онлайн по кадастровому номеру ${cadNumber} | Проверка объекта недвижимости онлайн по адресу: ${addressNotes}`}
+        keywords={`${cadNumber}, проверка недвижимости, ЕГРН, Росреестр`}
+      />
+      <InfoAppart cadastrObj={JSON.parse(cadastralObject)}/>
+    </div>
   )
 }
-
-// export async function getStaticPaths() {
-//   return {
-//     paths: [], //indicates that no page needs be created at build time
-//     fallback: 'blocking' //indicates the type of fallback
-//   }
-// }
-
-// export async function getStaticProps(context) {
-//   const info = context.params.cadnumber
-//   const getAskReestrByCudNum = await axios.get(`${cadastrUrl}${info}`)
-//   const reestrData = getAskReestrByCudNum.data
-//   console.log('REESTRBLYA', reestrData)
-//   const askObjectId = await axios(`${objectIdUrl}/${info}`)
-//   const objectId = askObjectId.data[0]?.objectId || null
-//   console.log('objectId', objectId)
-
-//   const getAskRights = await axios(`${egrpUrl}${objectId}`)
-//   const rights = getAskRights.data
-//   console.log('rights', rights)
-
-//   const oksType = reestrData.parcelData.oksType
-//   const address = reestrData.objectData.addressNote
-
-//   if (oksType === 'flat') {
-//     const url = `${priceUrl}${address}`
-//     const encodingUrl = encodeURI(url)
-//     const getAskPrice = await axios({
-//       headers: {
-//         'X-Api-Key': apiKey,
-//         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0',
-//         'Referer': 'https://xn--h1alcedd.xn--d1aqf.xn--p1ai/',
-//       },
-//       method: 'GET',
-//       timeout: 1000 * 10,
-//       url: encodingUrl
-//     })
-//     const flatPrice = getAskPrice.data
-//     console.log('PRICE', flatPrice)
-//     const lat = flatPrice?.bld?.pos?.lat
-//     const lng = flatPrice?.bld?.pos?.lng
-//     const infraUrl = `${socialUrl}${lat}&lng=${lng}`
-//     const encodingInfraUrl = encodeURI(infraUrl)
-
-//     const getAskStructure = await axios({
-//       headers: {
-//         'X-Api-Key': apiKey,
-//         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0',
-//         'Referer': 'https://xn--h1alcedd.xn--d1aqf.xn--p1ai/',
-//       },
-//       method: 'GET',
-//       timeout: 1000 * 10,
-//       url: encodingInfraUrl
-//     })
-//     const socialStructure = getAskStructure.data
-//     console.log('SOCIAL', socialStructure)
-//     return {
-//       props: {reestrData, objectId, rights, flatPrice, socialStructure}
-//     }
-//   }
-
-//   return {
-//     props: {reestrData, objectId, rights}
-//   }
-// }
 
 
 export async function getServerSideProps(context) {
@@ -105,7 +41,7 @@ export async function getServerSideProps(context) {
   const collection = db.collection('searchingObjects')
   const res = await collection.find({ $or : [{'objectData.objectCn': cadastr}, {'objectData.id':cadastr}]}).toArray()
   const cadastrObj = res[0]
-  const searchAdress = res[0].objectData?.objectAddress?.addressNotes || res[0].objectData?.objectAddress?.mergedAddress
+  const searchAdress = res?.[0].objectData?.objectAddress?.addressNotes || res[0].objectData?.objectAddress?.mergedAddress
   const searchFlat = res[0].dadata?.flat_type
   if (searchFlat !== null && searchAdress) {
     const regionFiasCode = res[0].dadata?.region_fias_id
