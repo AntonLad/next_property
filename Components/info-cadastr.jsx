ec1aecd5523f44e33fceff7a4ed1571c51b9e851
AntonLad <toxa.lada@gmail.com>
2022-02-22 16:51:45 +0300

    fix info-cadastr

--- Components/info-cadastr.jsx
+++ Components/info-cadastr.jsx
@@ -10,14 +10,9 @@ const Cadastr = ({ cadastrObj }) => {
   const { name, floor } = cadObj?.flat?.price?.flat ?? {}
   const rightType = cadObj?.rights?.realty?.rights
   const {
-    dateCreate, cadCost, dateCost, oksElementsConstructStr, oksFloors, oksUFloors, oksYearBuilt
+    dateCreate, cadCost, dateCost, oksElementsConstructStr, oksFloors, oksUFloors, oksYearBuilt, areaValue
   } = cadObj?.parcelData ?? {}
 
-  // const { regDate, infoUpdateDate } = useSelector((store) => store.common.getAskReestrByCudNum)
-
-  const { areaValue } = cadastrObj?.parcelData ?? {}
-
-  // console.log('+OBJ', objectName[0], 'TYPE', typeof objectName, '1 буква')
   return (
     <div data-content="kadastr" id="kadastr-info" className="object__block">
       <div className="object__block-wrap">
@@ -30,15 +25,17 @@ const Cadastr = ({ cadastrObj }) => {
               <b>{objectCn}</b>
             </div>
           </div>
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Адрес по документам:</div>
-            <div className="object__blockTableTd">{addressNotes}</div>
-          </div>
+          {addressNotes && addressNotes !== '0' && (
+            <div className="object__blockTableTr">
+              <div className="object__blockTableTd">Адрес по документам:</div>
+              <div className="object__blockTableTd">{addressNotes}</div>
+            </div>
+          )}
           {objectNameLetter !== '0' && (
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Тип:</div>
-            <div className="object__blockTableTd">{name || objectName}</div>
-          </div>
+            <div className="object__blockTableTr">
+              <div className="object__blockTableTd">Тип:</div>
+              <div className="object__blockTableTd">{name || objectName}</div>
+            </div>
           )}
           <div className="object__blockTableTr">
             <div className="object__blockTableTd">Тип собственности:</div>
@@ -46,63 +43,77 @@ const Cadastr = ({ cadastrObj }) => {
               <span>{rightType?.reduce((acc, rec) => rec.typeName, ' ') || 'не известно'}</span>
             </div>
           </div>
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Площадь:</div>
-            <div className="object__blockTableTd">{areaValue} кв.м.</div>
-          </div>
+          {areaValue && (
+            <div className="object__blockTableTr">
+              <div className="object__blockTableTd">Площадь:</div>
+              <div className="object__blockTableTd">{areaValue} кв.м.</div>
+            </div>
+          )}
           {floor && (
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Этаж:</div>
-            <div className="object__blockTableTd">{`${floor} этаж`}</div>
-          </div>
+            <div className="object__blockTableTr">
+              <div className="object__blockTableTd">Этаж:</div>
+              <div className="object__blockTableTd">{`${floor} этаж`}</div>
+            </div>
           )}
           {oksElementsConstructStr && (
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Матариалы стен:</div>
-            <div className="object__blockTableTd">{`${oksElementsConstructStr}`}</div>
-          </div>
+            <div className="object__blockTableTr">
+              <div className="object__blockTableTd">Матариалы стен:</div>
+              <div className="object__blockTableTd">{`${oksElementsConstructStr}`}</div>
+            </div>
           )}
           {oksFloors && (
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Этажность:</div>
-            <div className="object__blockTableTd">{oksFloors}</div>
-          </div>
+            <div className="object__blockTableTr">
+              <div className="object__blockTableTd">Этажность:</div>
+              <div className="object__blockTableTd">{oksFloors}</div>
+            </div>
           )}
           {oksUFloors && (
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Количество подземных этажей:</div>
-            <div className="object__blockTableTd">{oksUFloors}</div>
-          </div>
+            <div className="object__blockTableTr">
+              <div className="object__blockTableTd">Количество подземных этажей:</div>
+              <div className="object__blockTableTd">{oksUFloors}</div>
+            </div>
           )}
           {oksYearBuilt && (
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Год постройки:</div>
-            <div className="object__blockTableTd">{oksYearBuilt}</div>
-          </div>
-          )}
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Дата постановки на учёт:</div>
-            <div className="object__blockTableTd">{dateCreate}</div>
-          </div>
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Дата обновления информации:</div>
-            <div className="object__blockTableTd">{firActualDate}</div>
-          </div>
-        </div>
-        <div className="object__block-title-2">Кадастровая стоимость:</div>
-        <div className="clearfix"> </div>
-        <div className="object__blockTable">
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Кадастровая стоимость:</div>
-            <div className="object__blockTableTd">
-              <b>{cadCost} руб.</b>&nbsp;
+            <div className="object__blockTableTr">
+              <div className="object__blockTableTd">Год постройки:</div>
+              <div className="object__blockTableTd">{oksYearBuilt}</div>
             </div>
-          </div>
-          <div className="object__blockTableTr">
-            <div className="object__blockTableTd">Дата утверждения стоимости:</div>
-            <div className="object__blockTableTd">{dateCost}</div>
-          </div>
+          )}
+          {dateCreate && (
+            <div className="object__blockTableTr">
+              <div className="object__blockTableTd">Дата постановки на учёт:</div>
+              <div className="object__blockTableTd">{dateCreate}</div>
+            </div>
+          )}
+          {firActualDate && (
+            <div className="object__blockTableTr">
+              <div className="object__blockTableTd">Дата обновления информации:</div>
+              <div className="object__blockTableTd">{firActualDate}</div>
+            </div>
+           )}
         </div>
+        {cadCost && (
+          <>
+            <div className="object__block-title-2">Кадастровая стоимость:</div>
+            <div className="clearfix"> </div>
+            <div className="object__blockTable">
+              {cadCost && (
+                <div className="object__blockTableTr">
+                  <div className="object__blockTableTd">Кадастровая стоимость:</div>
+                  <div className="object__blockTableTd">
+                    <b>{cadCost} руб.</b>&nbsp;
+                  </div>
+                </div>
+              )}
+              {cadCost && (
+                <div className="object__blockTableTr">
+                  <div className="object__blockTableTd">Дата утверждения стоимости:</div>
+                  <div className="object__blockTableTd">{dateCost}</div>
+                </div>
+              )}
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
