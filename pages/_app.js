819335257e78883062101ae3a1afc097f4535536
dadhy <evgenzolotoff@gmail.com>
2022-04-09 12:05:06 +0400

    add liveinternet counter

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
        <Component {...pageProps} />
+       <script>
+        new Image().src = "https://counter.yadro.ru/hit?r"+
+        escape(document.referrer)+((typeof(screen)=="undefined")?"":
+        ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+        screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+        ";h"+escape(document.title.substring(0,150))+
+        ";"+Math.random();
+      </script>
     </Provider>
   )
 }
